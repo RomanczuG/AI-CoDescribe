a3d6b88378658c81f20c6aeddd953fe3ef8223d3
RomanczuG <romanczug@icloud.com>
2023-01-29 14:03:10 -0600

    pushing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,16 @@ import App from './App'
 import './index.css'
 import God from './components/God'
 import { BrowserRouter, Link } from 'react-router-dom'
+import Analytics from "analytics"
+import simpleAnalyticsPlugin from "@analytics/simple-analytics";
 
+const analytics = Analytics({
+  app: "codescribe.app",
+  plugins: [
+    // Load simple analytics! 🎉
+    simpleAnalyticsPlugin(),
+  ],
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
