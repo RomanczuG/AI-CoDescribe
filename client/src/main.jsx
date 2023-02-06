7e70780c8fded5477e419fe764919a2b23c89b3b
RomanczuG <romanczug@icloud.com>
2023-02-06 16:23:25 -0500

    pushing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,20 @@ import NotFound from './components/NotFound'
 import Layout from './components/Layout'
 import LandingPage from './components/LandingPage'
 
+let Content = React.lazy(() => import('./components/Layout'))
+
 ReactDOM.createRoot(document.getElementById('root')).render(  
   <React.StrictMode>
     <BrowserRouter>
         <Routes>
         <Route exact path="/" element = {<LandingPage/>}/>
+
           
-        <Route path="/app/*" element = {<Layout/>}/>
+        <Route path="/app/*" element = {
+        <React.Suspense fallback={<div>Loading...</div>}>
+        <Content/>
+        </React.Suspense>
+        }/>
           
         <Route path="*" element = {<NotFound/>}/>
       </Routes>
