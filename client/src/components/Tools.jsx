4d7b7d90479a5154bc96ede1c65b1a62cec5ee36
RomanczuG <romanczug@icloud.com>
2023-02-09 17:34:33 -0500

    pushing

--- client/src/components/Tools.jsx
+++ client/src/components/Tools.jsx
@@ -5,7 +5,7 @@ import React from "react";
 import axios from "axios";
 import { Listbox } from "@headlessui/react";
 import { Dialog } from "@headlessui/react";
-import Code from "./Code";
+// import Code from "./Code";
 
 
 const client = axios.create({
